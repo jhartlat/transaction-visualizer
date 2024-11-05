bb66f268704df2ed7ee5d9441acf89a16a7c0377
jhartlat <jhart.lat@gmail.com>
2024-11-05 00:56:38 -0600

    refeactor smallRow_2

--- modules/main.js
+++ modules/main.js
@@ -68,27 +68,28 @@ function createVisualizer(remainingBalance) {
 
 function smallRow_1(mainColumn) {
     const row_1 = mainColumn.addStack();
+
     // Card Name Label
     const cardNameLabel = row_1.addText(CARD_NAME + '\n');
     cardNameLabel.font = STYLE.font.row_1;
-    row_1.addSpacer();
+
     // Spacer after row 1
     mainColumn.addSpacer();
 }
 
 function smallRow_2(mainColumn, remainingBalance) {
     const row_2 = mainColumn.addStack();
-    // Determine the balance amount and font based on CARD_TYPE
+
+    // Balance Label
     const isCurrentBalance = CARD_TYPE === "CURRENT BALANCE";
     const balanceAmount = isCurrentBalance ? TOTAL_SPENT : remainingBalance;
     const balanceText = formatCurrency(balanceAmount, DEVICE_LOCALE, CURRENCY_CODE);
-    // Add the balance text to row_2
     const balanceLabel = row_2.addText(balanceText);
-    // Set font and text color
     balanceLabel.font = STYLE.font.row_2;
     if (!isCurrentBalance) {
         balanceLabel.textColor = getBalanceColor(remainingBalance);
     }
+    
     // Spacer after row 2
     mainColumn.addSpacer();
 }
@@ -166,7 +167,7 @@ function daysBetweenDates(currentDate, closingDate) {
 }
 
 
-function mediumRow_1() {
+function mediumRow_1(mainColumn) {
     const row_1 = mainColumn.addStack();
 
     // Card Name Label
@@ -184,16 +185,111 @@ function mediumRow_1() {
     mainColumn.addSpacer();
 }
 
-function mediumRow_2() {
-    
+function mediumRow_2(mainColumn, remainingBalance) {
+    const row_2 = mainColumn.addStack();
 
+    if (CARD_TYPE === "CURRENT BALANCE") {
+        const totalBalance = formatCurrency(TOTAL_SPENT, DEVICE_LOCALE, CURRENCY_CODE);
+        const totalLabel = row_2.addText(totalBalance);
+    } else {
+        const formattedBalance = formatCurrency(remainingBalance, DEVICE_LOCALE, CURRENCY_CODE);
+        const balanceLabel = row_2.addText(formatttedBalance);
+        balanceLabel.textColor = getBalanceColor(remainingBalance);
+        balanceLabel.font = STYLE.font.row_2;
+    }
+    mainColumn.addSpacer();
 }
 
-function mediumRow_3() {
+function mediumRow_3(mainColumn) {
+    const row_3 = mainColumn.addStack();
 
+    // Card Type Background
+    const cardTypeBackground = row_3.addStack();
+    cardTypeBackground.cornerRadius = 5;
+    cardTypeBackground.setPadding(5, 10, 5, 10);
+    cardTypeBackground.backgroundColor = new Color(generateMonochromatic(BACKGROUND_COLOR));
+
+    // Alignment Stack
+    const alignmentStack = row_3.addStack();
+    alignmentStack.setPadding(5, 10, 5, 10);
+
+    // Card Type Label
+
+    if (CARD_TYPE == "CURRENT BALANCE") {
+        let card_type = CARD_TYPE;
+        let cardTypeLabel = cardTypeBackground.addText(`${card_type}`);
+        cardTypeLabel.font = STYLE.font.row_3;
+        mainColumn.addSpacer();
+        return;
+    }
+    let cardTypeLabel = cardTypeBackground.addText(`${CARD_TYPE}`);
+    cardTypeLabel.font = STYLE.font.row_3;
+
+    // Checking Label
+    let checkingLabel;
+    if (CHECKING_ACCOUNT == MONTHLY_LIMIT) {
+        checkingLabel = alignmentStack.addText(`CHK: MAX `);
+        checkingLabel.textColor = STYLE.color.greyedOut;
+    } else {
+        let formattedChecking = formatCurrency(CHECKING_ACCOUNT, DEVICE_LOCALE, CURRENCY_CODE);
+        checkingLabel = alignmentStack.addText(`CHK: ${formattedChecking} `);
+    }
+    checkingLabel.font = STYLE.font.row_3;
+
+    // Savings Label
+    let savingsLabel;
+    if (SAVINGS_ACCOUNT == 0) {
+        savingsLabel = alignmentStack.addText('SAV: ←');
+        savingsLabel.textColor = STYLE.color.greyedOut;
+    } else {
+        let formattedSavings = formatCurrency(SAVINGS_ACCOUNT, DEVICE_LOCALE, CURRENCY_CODE);
+        savingsLabel = alignmentStack.addText(`SAV: ${formattedSavings}`);
+        savingsLabel.textColor = STYLE.color.negativeBalance;
+    }
+    savingsLabel.font = STYLE.font.row_3;
+
+    // Row 3 complete
+    mainColumn.addSpacer();
 }
 
 function mediumRow_4() {
+    function addRow_4(mainColumn) {
+        const row_4 = mainColumn.addStack();
+
+        // Last Activity Label
+        const lastActivityLabel = row_4.addText("Last Activity: ");
+        lastActivityLabel.font = STYLE.font.row_4;
+
+
+        // Activity Amount
+        let formattedAmount = RECENT;
+
+        if (typeof (RECENT) == "number") {
+
+            if (RECENT > 0) {
+                formattedAmount = '-' + formatCurrency(RECENT, DEVICE_LOCALE, CURRENCY_CODE);
+            } else {
+                formattedAmount = '+' + formatCurrency((RECENT * -1), DEVICE_LOCALE, CURRENCY_CODE);
+            }
+        }
+
+        // Amount Label
+        const amountLabel = row_4.addText(formattedAmount);
+        if (formattedAmount == 'N/A') {
+            amountLabel.textColor = STYLE.color.greyedOut;
+        }
+        amountLabel.font = STYLE.font.row_4
+        row_4.addSpacer();
+
+        // Current Time Label
+        const currentTime = getTime();
+        const currentTimeLabel = row_4.addText(currentTime);
+        currentTimeLabel.font = STYLE.font.row_4;
+
+        // Row 4 complete
+        mainColumn.addSpacer();
+    }
+
 
 }
 
